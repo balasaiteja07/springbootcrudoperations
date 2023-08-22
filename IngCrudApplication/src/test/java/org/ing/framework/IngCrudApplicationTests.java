90b36fccfd7fb1a0b0efc285c97dbe846156e587
Sai Kumar <saiteja.tkr07@gmail.com>
2023-08-22 23:11:17 +0530

    initial commit

---
+++ IngCrudApplication/src/test/java/org/ing/framework/IngCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ing.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
