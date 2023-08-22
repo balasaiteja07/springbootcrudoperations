e0d7b3586638d53eec62b0eed2e46cafff175102
Sai Kumar <saiteja.tkr07@gmail.com>
2023-08-22 23:19:15 +0530

    added  model class

---
+++ IngCrudApplication/src/main/java/org/ing/framework/model/Employee.java
@@ -0,0 +1,19 @@
+package org.ing.framework.model;
+
+import java.util.HashMap;
+
+
+public class Employee {
+	
+	private long id;
+	
+	private String name;
+	private int age;
+	private long mobileNo;
+	private double salary;
+	private HashMap<String, String> address; // Prakasam, Koppole
+	
+	
+	
+
+}
